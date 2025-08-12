7de08178b047d9fa492f71bc03b6006f9511db77
Borillion <borillion_star@yahoo.com>
2025-08-11 23:44:59 -0700

    Add initial implementation of CaliScan module and configuration files

---
+++ CaliScan.py
@@ -0,0 +1,38 @@
+## Refering to https://dynamicmacros.3dcoded.xyz/extras/extras-intro/
+## Klipper looks for sections in printer.cfg to load defined in [name]
+## It loads the extras name.py from ~/klipper/klippy/extras/name.py
+
+# [caliscan]
+# host: 127.0.0.1
+# port: 9880
+# grid_min
+# grid_max
+
+class CaliScan:
+    def __init__(self, config):
+        # Get the printer config from klipper
+        self.printer = config.get_printer()
+        self.gcode = self.printer.lookup_object('gcode')
+        self.reactor = self.printer.get_reactor()
+
+        # Lets start by sending a message to the console
+        self.message = config.get('message', 'Loaded CaliScan Module')
+
+        # Schedule a one-time greeting right after Klipper starts
+        waketime = self.reactor.monotonic() + 1
+        self.reactor.register_timer(self._greet, waketime)
+
+        # Still register the G-code in case you want it later
+        self.gcode.register_command('CALI_SCAN', self._ready_handler)
+
+    def _ready_handler(self, gcmd):
+        waketime = self.reactor.monotonic() + 1
+        self.reactor.register_timer(self._greet, waketime)
+
+    def _greet(self, eventtime=None):
+        self.gcode.respond_info(self.message)
+        return self.reactor.NEVER  # Do not re-run timer
+
+def load_config(config):
+    # Entry point for Klippy to load this extra
+    return CaliScan(config)
