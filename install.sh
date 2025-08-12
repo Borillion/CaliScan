d29c3aacceb1a060c2a30dfe289391c5417a9092
Borillion <borillion_star@yahoo.com>
2025-08-12 00:28:44 -0700

    Fix typo in installation script for caliscan.py symlink path

--- install.sh
+++ install.sh
@@ -12,14 +12,13 @@ if [ ! -d "$KLIPPER_DIR" ]; then
 fi
 
 # update link to caliscan.py 
-echo "caliscan: linking klippy to caliscan.py"
 if [ -e "${KLIPPER_DIR}/klippy/extras/caliscan.py" ]; then
     rm "${KLIPPER_DIR}/klippy/extras/caliscan.py"
 fi
 
 # Link caliscan.py from project into Klipper extras
-echo "caliscan: linking klippy to ${BKDIR}/caliscan.py"
-ln -sfn "${BKDIR}/caliscan.py" "${KLIPPER_DIR}/klippy/extras/caliscan.py"
+echo "caliscan: linking klippy to ${CURR_DIR}/caliscan.py"
+ln -sfn "${BKDCURR_DIRIR}/caliscan.py" "${KLIPPER_DIR}/klippy/extras/caliscan.py"
 
 # Exclude py from git backup and tracking
 if ! grep -q "klippy/extras/caliscan.py" "${KLIPPER_DIR}/.git/info/exclude"; then
